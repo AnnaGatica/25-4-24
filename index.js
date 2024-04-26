cd770ca8b685e9871296c3fb41be862c18a024dd
AnnaGatica <166068871+AnnaGatica@users.noreply.github.com>
2024-04-26 20:31:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//1)
+letContraseña=prompt("Coloca tu contraseña")
+letUsuario=prompt("Ingresa tu usuario")
+
+if((Contraseña=="juanGarcia23")&&
+(Usuario=="cJZoht3!")) {
+     alert("Bienvenido + Usuario")
+{else}
+     alert("Contraseña y Usuario incorrecto")
+}
+
+
+//2) 
+letNotaExamen= Number(prompt)("¿Que calificación sacaste?")
+letNotaTP= Number(prompt)("¿Que calificacion te sacaste en los tp?")
+letAsistencia=prompt("¿Tienes asistencia completa?")
+
+if((NotaExamen>8)&&(NotaTP>=6) 
+(Asistencia==True)) {
+    alert("Aprobaste la materia")
+{else}
+    alert("Desaprobaste la materia")
+}
+
+//3) 
+letLunesMiercolesViernes="Basura Humeda"
+letMartesJuevesDomingo="Basura Seca"
+letSabado="No se saca la basura"
+
+if((LunesMiercolesViernes=="Basura Humeda")&&(MartesJuevesDomingo=="Basura Seca")&& (Sabado=="No se saca basura")){
+    alert("Los lunes, Miercoles y Viernes sacar la basura humeda; Los Martes, Jueves y Domingos sacar la basura seca y los Sabados no se saca")
+{else}
+
+
+}
